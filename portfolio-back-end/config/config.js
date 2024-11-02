dc50b7ef87e37cfa269a4b91aefb57cd23c8bea8
Ricardo199 <ricardo1529@gmail.com>
2024-11-02 13:06:51 -0400

    Add dotenv for environment variable management and update MongoDB connection string

--- portfolio-back-end/config/config.js
+++ portfolio-back-end/config/config.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+const password = process.env.DB_PASSWORD;
+
 module.exports = {
-    "ATLASDB":"mongodb+srv://ricardo1529:Cs5y6Hm7LClWNe1C@midterm.fuok2.mongodb.net/?retryWrites=true&w=majority&appName=Midterm"
+    "ATLASDB":"mongodb+srv://ricardo1529:${password}@midterm.fuok2.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Midterm"
 };
